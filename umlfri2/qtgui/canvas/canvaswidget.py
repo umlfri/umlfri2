0a076373f5ca357c9dae2715b8b2d19d25829992
Jan Janech <janik@janik.ws>
2015-11-13 19:56:20 +0100

    selecting using CTRL

--- umlfri2/qtgui/canvas/canvaswidget.py
+++ umlfri2/qtgui/canvas/canvaswidget.py
@@ -1,6 +1,5 @@
 from PySide.QtCore import Qt
-from PySide.QtGui import QWidget, QPainter
-
+from PySide.QtGui import QWidget, QPainter, QApplication
 from ..base.qtruler import QTRuler
 from .qtpaintercanvas import QTPainterCanvas
 from umlfri2.types.geometry import Point
@@ -25,11 +24,13 @@ class CanvasWidget(QWidget):
         return self.__ruler
     
     def mousePressEvent(self, event):
-        # TODO: for testing purposes only
         pos = event.pos()
         object = self.__tab.diagram.get_visual_at(self.__ruler, Point(pos.x(), pos.y()))
-        if object is None:
-            self.__tab.selection.deselect_all()
+        
+        if QApplication.keyboardModifiers() == Qt.ControlModifier:
+            if object is not None:
+                self.__tab.selection.toggle_select(object)
         else:
-            self.__tab.selection.toggle_select(object)
+            self.__tab.selection.select(object)
+        
         self.repaint()
