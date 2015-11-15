6f35468ee8732bb12ca130628f3e19838c1a598a
Jan Janech <janik@janik.ws>
2015-11-15 23:36:41 +0100

    using non unique names instead of empty string in generator

--- umlfri2/ufl/types/uniquevaluegenerator.py
+++ umlfri2/ufl/types/uniquevaluegenerator.py
@@ -10,7 +10,7 @@ class UniqueValueGenerator:
         while True:
             new = template.get_text(context.extend(i, 'no'))
             if new == prev:
-                return "" # cant find template
+                return new # cant find template
             
             if not self.has_value(new):
                 return new
