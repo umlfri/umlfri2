914e88fe0910201e9ac1bcb2778dac58e080029e
Jan Janech <janik@janik.ws>
2015-11-17 22:27:23 +0100

    QIcon-s are stored in in-memory cache for later use, instead of repeated loading from file

--- umlfri2/qtgui/base/image_loader.py
+++ umlfri2/qtgui/base/image_loader.py
@@ -1,4 +1,9 @@
+from weakref import WeakKeyDictionary
+
 from PySide.QtGui import QPixmap, QIcon
+
+
+icon_cache = WeakKeyDictionary()
 
 
 def load(image):
@@ -8,4 +13,9 @@ def load(image):
 
 
 def load_icon(image):
-    return QIcon(load(image))
+    storage_icon_cache = icon_cache.setdefault(image.storage, {})
+    if image.path in storage_icon_cache:
+        return storage_icon_cache[image.path]
+    icon = QIcon(load(image))
+    storage_icon_cache[image.path] = icon
+    return icon
