6c70e48a0c51644864c0d13566b32ecd716443b7
Jan Janech <janik@janik.ws>
2015-11-17 15:38:28 +0100

    properties widget is using tabs

--- umlfri2/qtgui/mainwindow/propertieswidget.py
+++ umlfri2/qtgui/mainwindow/propertieswidget.py
@@ -1,9 +1,13 @@
-from PySide.QtGui import QTableWidget
+from PySide.QtGui import QTableWidget, QTabWidget
 
 
-class PropertiesWidget(QTableWidget):
+class PropertiesWidget(QTabWidget):
     def __init__(self):
         super().__init__()
-        self.setColumnCount(2)
-        self.setHorizontalHeaderLabels(["Name", "Value"])
-        self.horizontalHeader().setStretchLastSection(True)
+        
+        self.__table = QTableWidget()
+        self.__table.setColumnCount(2)
+        self.__table.setHorizontalHeaderLabels(["Name", "Value"])
+        self.__table.horizontalHeader().setStretchLastSection(True)
+        self.setTabPosition(QTabWidget.South)
+        self.addTab(self.__table, "Properties")
