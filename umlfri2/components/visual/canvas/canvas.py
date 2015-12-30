5f60420efa5fa86ba6dd65af325dc933f9c764fc
Jan Janech <janik@janik.ws>
2015-12-30 13:03:09 +0100

    false documentation removed

--- umlfri2/components/visual/canvas/canvas.py
+++ umlfri2/components/visual/canvas/canvas.py
@@ -1,27 +1,9 @@
 import abc
-from umlfri2.types.color import Color
-from .ruler import Ruler
 
 
 class Canvas(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def draw_ellipse(self, rectangle, fg=None, bg=None, line_width=None, line_style=None):
-        """
-        Draws ellipse to canvas.
-        
-        :param pos: Position of the ellipse top left corner
-        :type pos: (int, int)
-        :param size: Size of the ellipse
-        :type size: (int, int)
-        :param fg: Foreground color
-        :type fg: Color
-        :param bg: Background color
-        :type bg: Color
-        :param line_width: Width of the arc outline
-        :type line_width: int
-        :param line_style: Line style used for arc outline
-        :type line_style: int
-        """
         pass
     
     @abc.abstractmethod
@@ -50,9 +32,4 @@ class Canvas(metaclass=abc.ABCMeta):
     
     @abc.abstractmethod
     def get_ruler(self):
-        """
-        Get ruler associated with current canvas.
-        :return Ruler
-        :rtype Ruler
-        """
         pass
