ea230a85e919e68a6111f71b798bd414d0b51a31
Jan Janech <janik@janik.ws>
2016-01-02 21:13:15 +0100

    accessing project node from ufl (in element appearance only for now)

--- umlfri2/metamodel/elementtype.py
+++ umlfri2/metamodel/elementtype.py
@@ -1,6 +1,36 @@
 from weakref import ref
 
 from umlfri2.components.base.context import Context
+from umlfri2.ufl.types import UflType, UflListType, UflImageType, UflStringType
+
+
+class UflNodeType(UflType):
+    pass
+
+
+UflNodeType.ALLOWED_DIRECT_ATTRIBUTES = {
+    'children': ('children', UflListType(UflNodeType)),
+    'icon': ('icon', UflImageType()),
+    'label': ('label', UflStringType()),
+}
+
+
+class UflNode:
+    def __init__(self, element):
+        self.__element = element
+    
+    @property
+    def children(self):
+        for child in self.__element.children:
+            yield UflNode(child)
+    
+    @property
+    def icon(self):
+        return self.__element.type.icon
+    
+    @property
+    def label(self):
+        return self.__element.get_display_name()
 
 
 class ElementType:
@@ -33,15 +63,22 @@ class ElementType:
         return self.__ufl_type
     
     def compile(self):
-        variables = {'self': self.__ufl_type, 'cfg': self.__metamodel().addon.config_structure}
+        variables = {
+            'self': self.__ufl_type,
+            'cfg': self.__metamodel().addon.config_structure,
+        }
         
-        self.__appearance.compile(variables)
+        appearance_variables = variables.copy()
+        appearance_variables.update(node=UflNodeType())
+        
+        self.__appearance.compile(appearance_variables)
         self.__display_name.compile(variables)
     
     def create_appearance_object(self, element, ruler):
         context = Context()\
             .extend(element.data, 'self')\
-            .extend(self.__metamodel().addon.config, 'cfg')
+            .extend(self.__metamodel().addon.config, 'cfg')\
+            .extend(UflNode(element), 'node')
         return self.__appearance.create_visual_object(context, ruler)
     
     def get_display_name(self, element):
