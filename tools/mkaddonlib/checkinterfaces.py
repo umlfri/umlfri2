a4c440bfdfa199b78921c91180a605db36a58c87
Jan Janech <janik@janik.ws>
2016-01-09 14:16:02 +0100

    command line interface for checkinterfaces script

--- tools/mkaddonlib/checkinterfaces.py
+++ tools/mkaddonlib/checkinterfaces.py
@@ -1,14 +1,30 @@
+from optparse import OptionParser
+
+import sys
+
 from lib.checker.writer import UmlFriInterfaceWriter
 from lib.model import Builder
 from lib.checker import UmlFriInterfaceLoader
 
-builder = Builder()
 
-builder.parse()
-builder.finish()
+def main(*args):
+    options_parser = OptionParser(usage = "usage: %prog [options]")
+    options_parser.add_option("-o", "--output", dest="output_directory", default="output/",
+                      help="write output to DIR", metavar="DIR")
+    
+    (options, args) = options_parser.parse_args(list(args))
+    
+    builder = Builder()
+    
+    builder.parse()
+    builder.finish()
+    
+    loader = UmlFriInterfaceLoader()
+    
+    interfaces = loader.load()
+    interfaces = interfaces.fix_from(builder.get_type_by_type('Interface'))
+    
+    writer = UmlFriInterfaceWriter(interfaces)
+    writer.write_to(options.output_directory)
 
-loader = UmlFriInterfaceLoader()
-interfaces = loader.load()
-interfaces = interfaces.fix_from(builder.get_type_by_type('Interface'))
-writer = UmlFriInterfaceWriter(interfaces)
-writer.write_to('checked_api')
+main(*sys.argv[1:])
