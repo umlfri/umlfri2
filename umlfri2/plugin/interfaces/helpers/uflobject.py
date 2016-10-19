2f89b0b951f3acdf902990cab704a5907a9c8f64
Jan Janech <janik@janik.ws>
2016-10-19 17:54:10 +0200

    font and color information can be sent to plugin through public api

--- umlfri2/plugin/interfaces/helpers/uflobject.py
+++ umlfri2/plugin/interfaces/helpers/uflobject.py
@@ -1,4 +1,4 @@
-from umlfri2.ufl.types import UflObjectType, UflListType
+from umlfri2.ufl.types import UflObjectType, UflListType, UflColorType, UflFontType
 
 
 class UflObjectApiHelper:
@@ -21,7 +21,7 @@ class UflObjectApiHelper:
                 elif isinstance(attribute_type, UflListType):
                     yield from list_recursion(attribute_path, attribute_type, attribute_value)
                 else:
-                    yield attribute_path, attribute_value
+                    yield attribute_path, self.__convert_get(attribute_type, attribute_value)
         
         def list_recursion(path, type, list):
             item_type = type.item_type
@@ -35,9 +35,18 @@ class UflObjectApiHelper:
                 elif isinstance(item_type, UflListType):
                     yield from list_recursion(item_path, item_type, item_value)
                 else:
-                    yield item_path, item_value
+                    yield item_path, self.__convert_get(item_type, item_value)
         
         return object_recursion(None, self.__ufl_object.type, self.__ufl_object)
+    
+    def __convert_get(self, type, value):
+        if isinstance(type, (UflObjectType, UflListType)):
+            raise Exception("Invalid type - can return only primitive types")
+        
+        if isinstance(type, (UflColorType, UflFontType)):
+            return str(value)
+        
+        return value
     
     def __getitem__(self, item):
         path = item.split('/')
@@ -54,4 +63,4 @@ class UflObjectApiHelper:
             else:
                 raise Exception
         
-        return obj
+        return self.__convert_get(type, obj)
