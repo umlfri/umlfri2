ed32079a2c28538c8b53b8319821b25df1750917
Jan Janech <janik@janik.ws>
2017-03-21 15:54:34 +0100

    old version contributions in about dialog

--- umlfri2/application/about.py
+++ umlfri2/application/about.py
@@ -11,7 +11,9 @@ class AboutUmlFri:
     name = "UML .FRI"
     version = Version("2.0")
     
-    author = "© 2015-2017 Ján Janech"
+    @property
+    def author(self):
+        yield "Ján Janech", (2015, 2017)
     
     @property
     def description(self):
@@ -24,3 +26,28 @@ class AboutUmlFri:
         yield 'PyQt', Qt.PYQT_VERSION_STR
         yield 'AppDirs', appdirs.__version__
         yield 'lxml', lxml.etree.__version__
+    
+    @property
+    def version_1_contributions(self):
+        yield 'Lubomir Sadlon', (2005, 2008)
+        yield 'Jan Janech', (2005, 2015)
+        yield 'Pavol Kovalik', (2005, 2007)
+        yield 'Marek Dovjak', (2005, 2007)
+        yield 'Miroslav Spigura', (2005, 2007)
+        yield 'Martina Iskerkova', (2006, 2008)
+        yield 'Tomas Baca', (2007, 2011)
+        yield 'Milan Juricek', (2007, 2009)
+        yield 'Pavol Odlevak', (2007, 2009)
+        yield 'Slavomir Kavecky', (2008, 2010)
+        yield 'Michal Kovacik', (2008, 2010)
+        yield 'Martin Kubincanek', (2008, 2011)
+        yield 'Peter Pajta', (2008, 2010)
+        yield 'Katarina Stanikova', (2008, 2010)
+        yield 'Peter Turza', (2008, 2010)
+        yield 'Milos Korenciak', (2009, 2010)
+        yield 'Tomas Chabada', (2009, 2010)
+        yield 'Marian Kubincanek', (2009, 2010)
+        yield 'Igor Bartak', (2010, 2011)
+        yield 'Jozef Harabin', (2010, 2012)
+        yield 'Matej Jancura', (2011, 2012)
+        yield 'Matej Melko', (2011, 2012)
--- umlfri2/qtgui/mainwindow/aboutdialog.py
+++ umlfri2/qtgui/mainwindow/aboutdialog.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QDialog, QHBoxLayout, QVBoxLayout, QLabel, QSpacerItem, QDialogButtonBox, QTabWidget,\
     QTextEdit
 from PyQt5.QtWidgets import QFormLayout
+from PyQt5.QtWidgets import QScrollArea
 from PyQt5.QtWidgets import QWidget
 
 from umlfri2.application import Application
@@ -38,7 +39,8 @@ class About(QDialog):
         desc_label.setWordWrap(True)
         desc_text_layout.addWidget(desc_label)
         desc_text_layout.addStretch(1)
-        desc_text_layout.addWidget(QLabel("<small>{0}</small>".format(Application().about.author)))
+        for author, year in Application().about.author:
+            desc_text_layout.addWidget(QLabel("<small>{0}</small>".format(self.__about_line(author, year))))
         
         desc_layout.addLayout(desc_text_layout, 1)
         
@@ -64,6 +66,17 @@ class About(QDialog):
             license_text.setLineWrapMode(QTextEdit.NoWrap)
             
             tabs.addTab(license_text, _("License"))
+        
+        version_1_layout = QVBoxLayout()
+        
+        for author, year in Application().about.version_1_contributions:
+            version_1_layout.addWidget(QLabel(self.__about_line(author, year)))
+        
+        version_1_widget = QWidget()
+        version_1_widget.setLayout(version_1_layout)
+        version_1_scroll = QScrollArea()
+        version_1_scroll.setWidget(version_1_widget)
+        tabs.addTab(version_1_scroll, _("Old version contributions"))
 
         button_box = QDialogButtonBox(QDialogButtonBox.Ok)
         button_box.button(QDialogButtonBox.Ok).setText(_("Ok"))
@@ -72,3 +85,12 @@ class About(QDialog):
         main_layout.addWidget(button_box)
         
         self.setLayout(main_layout)
+
+    def __about_line(self, author, year):
+        line = "© "
+        if isinstance(year, tuple):
+            line += "{0}-{1} ".format(year[0], year[1])
+        else:
+            line += "{0} ".format(author[1])
+        line += author
+        return line
