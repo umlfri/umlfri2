e8e7452d55198ae7934265eab20121e73191a764
Jan Janech <janik@janik.ws>
2017-03-23 12:57:34 +0100

    gtk icon theme is correctly applied if XDG environment variables are correctly set, so the gtkstylefix is not needed

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from PyQt5.QtWidgets import QApplication
 
 from umlfri2.application import Application
 from umlfri2.qtgui import SplashScreen
-from umlfri2.qtgui.base.gtkstylefix import set_gtk_icon_theme_if_needed
 from umlfri2.qtgui.base.qtthreadmanager import QTThreadManager
 from umlfri2.qtgui.exceptionhook import install_exception_hook
 from umlfri2.qtgui.rendering import QTRuler
@@ -16,8 +15,6 @@ def main(args):
     apply_os_specials()
     
     app = QApplication(args)
-    
-    set_gtk_icon_theme_if_needed()
     
     Application().use_thread_manager(QTThreadManager())
     Application().use_ruler(QTRuler())
