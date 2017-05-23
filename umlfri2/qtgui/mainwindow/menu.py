d60567b831e5cb2109ebd931d6beadb5fa0130c0
Jan Janech <janik@janik.ws>
2017-05-23 13:07:40 +0200

    recent files have only a base name shown in the application menu, full path is included in tooltip instead

--- umlfri2/qtgui/mainwindow/menu.py
+++ umlfri2/qtgui/mainwindow/menu.py
@@ -176,9 +176,12 @@ class MainWindowMenu(QMenuBar):
         self.__main_window.open_solution()
     
     def __recent_files_menu_populate(self):
+        self.__recent_files_menu.setToolTipsVisible(True)
         self.__recent_files_menu.clear()
         for no, file in enumerate(reversed(list(Application().recent_files))):
-            action = self.__recent_files_menu.addAction("&{0}. {1}".format(no, file))
+            file_name = os.path.basename(file)
+            action = self.__recent_files_menu.addAction("&{0}. {1}".format(no, file_name))
+            action.setToolTip(file)
             action.triggered.connect(partial(self.__main_window.open_solution_from_file, file))
     
     def __file_save_action(self, checked=False):
