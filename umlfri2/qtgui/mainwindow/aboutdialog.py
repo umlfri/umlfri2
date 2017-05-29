a07a333c43e6d3c3e3c742ba61df2f03c9e3df66
Jan Janech <janik@janik.ws>
2017-05-29 12:39:02 +0200

    about dialog refactoring

--- umlfri2/qtgui/mainwindow/aboutdialog.py
+++ umlfri2/qtgui/mainwindow/aboutdialog.py
@@ -20,7 +20,7 @@ class About(QDialog):
         main_layout = QVBoxLayout()
         
         desc_layout = QHBoxLayout()
-
+        
         logo_pixmap = QPixmap()
         logo_pixmap.load(os.path.join(GRAPHICS, "logo", "logo_full.png"))
         
@@ -48,63 +48,14 @@ class About(QDialog):
         tabs = QTabWidget()
         main_layout.addWidget(tabs, 1)
         
-        versions_layout = QFormLayout()
-        
-        for depencency, version in Application().about.dependency_versions:
-            versions_layout.addRow(_("{0} version").format(depencency), QLabel(version))
-        
-        versions_widget = QWidget()
-        versions_widget.setLayout(versions_layout)
-        tabs.addTab(versions_widget, _("Used libraries"))
+        tabs.addTab(self.create_used_libraries_tab(), _("Used libraries"))
         
         if os.path.exists(LICENSE_FILE):
-            license_text = QTextEdit()
-            with open(LICENSE_FILE, 'rt') as license_file:
-                license_text.setPlainText(license_file.read())
-            license_text.setReadOnly(True)
-            license_text.setLineWrapMode(QTextEdit.NoWrap)
-            
-            tabs.addTab(license_text, _("License"))
+            tabs.addTab(self.create_license_tab(), _("License"))
         
-        version_1_layout = QVBoxLayout()
-        
-        for author, year in Application().about.version_1_contributions:
-            version_1_layout.addWidget(QLabel(self.__about_line(author, year)))
-        
-        version_1_widget = QWidget()
-        version_1_widget.setLayout(version_1_layout)
-        version_1_scroll = QScrollArea()
-        version_1_scroll.setWidget(version_1_widget)
-        tabs.addTab(version_1_scroll, _("Version 1.0 contributions"))
-        
-        updates_layout = QGridLayout()
-        updates_widget = QWidget()
-        updates_widget.setLayout(updates_layout)
-        
-        check_updates = QPushButton(_("Check for updates"))
-        updates_layout.addWidget(check_updates, 0, 0)
+        tabs.addTab(self.create_version_1_0_tab(), _("Version 1.0 contributions"))
 
-        updates_layout.addWidget(QLabel(_("Latest version available:")), 1, 0)
-        if Application().about.updates.latest_version is None:
-            updates_layout.addWidget(QLabel("-"), 1, 1)
-        else:
-            updates_layout.addWidget(QLabel(str(Application().about.updates.latest_version)), 1, 1)
-        
-        if Application().about.updates.has_newer_version:
-            updates_layout.addWidget(QLabel("<a href=\"{0}\">{1}</a>".format(
-                Application().about.updates.version_update_url, _("download update")
-            )), 1, 2)
-
-        if Application().about.updates.latest_prerelease is not None:
-            updates_layout.addWidget(QLabel(_("Latest unstable version available:")), 2, 0)
-            updates_layout.addWidget(QLabel(str(Application().about.updates.latest_prerelease)), 2, 1)
-    
-            if Application().about.updates.has_newer_version:
-                updates_layout.addWidget(QLabel("<a href=\"{0}\">{1}</a>".format(
-                    Application().about.updates.prerelease_update_url, _("download update")
-                )), 2, 2)
-        
-        tabs.addTab(updates_widget, _("Updates"))
+        tabs.addTab(self.create_updates_tab(), _("Updates"))
         
         if Application().about.is_debug_version:
             debug_layout = QHBoxLayout()
@@ -114,7 +65,7 @@ class About(QDialog):
             debug_text = QLabel(_("You are running UML .FRI in the debug mode"))
             debug_layout.addWidget(debug_text, 1)
             main_layout.addLayout(debug_layout)
-
+        
         button_box = QDialogButtonBox(QDialogButtonBox.Ok)
         button_box.button(QDialogButtonBox.Ok).setText(_("Ok"))
         button_box.accepted.connect(self.accept)
@@ -122,7 +73,58 @@ class About(QDialog):
         main_layout.addWidget(button_box)
         
         self.setLayout(main_layout)
+    
+    def create_used_libraries_tab(self):
+        versions_layout = QFormLayout()
+        for depencency, version in Application().about.dependency_versions:
+            versions_layout.addRow(_("{0} version").format(depencency), QLabel(version))
+        versions_widget = QWidget()
+        versions_widget.setLayout(versions_layout)
+        return versions_widget
+    
+    def create_license_tab(self):
+        license_text = QTextEdit()
+        with open(LICENSE_FILE, 'rt') as license_file:
+            license_text.setPlainText(license_file.read())
+        license_text.setReadOnly(True)
+        license_text.setLineWrapMode(QTextEdit.NoWrap)
+        return license_text
+    
+    def create_version_1_0_tab(self):
+        version_1_layout = QVBoxLayout()
+        for author, year in Application().about.version_1_contributions:
+            version_1_layout.addWidget(QLabel(self.__about_line(author, year)))
+        version_1_widget = QWidget()
+        version_1_widget.setLayout(version_1_layout)
+        version_1_scroll = QScrollArea()
+        version_1_scroll.setWidget(version_1_widget)
+        return version_1_scroll
+    
+    def create_updates_tab(self):
+        updates_layout = QGridLayout()
+        updates_widget = QWidget()
+        updates_widget.setLayout(updates_layout)
+        check_updates = QPushButton(_("Check for updates"))
+        updates_layout.addWidget(check_updates, 0, 0)
+        updates_layout.addWidget(QLabel(_("Latest version available:")), 1, 0)
+        if Application().about.updates.latest_version is None:
+            updates_layout.addWidget(QLabel("-"), 1, 1)
+        else:
+            updates_layout.addWidget(QLabel(str(Application().about.updates.latest_version)), 1, 1)
+        if Application().about.updates.has_newer_version:
+            updates_layout.addWidget(QLabel("<a href=\"{0}\">{1}</a>".format(
+                Application().about.updates.version_update_url, _("download update")
+            )), 1, 2)
+        if Application().about.updates.latest_prerelease is not None:
+            updates_layout.addWidget(QLabel(_("Latest unstable version available:")), 2, 0)
+            updates_layout.addWidget(QLabel(str(Application().about.updates.latest_prerelease)), 2, 1)
 
+            if Application().about.updates.has_newer_version:
+                updates_layout.addWidget(QLabel("<a href=\"{0}\">{1}</a>".format(
+                    Application().about.updates.prerelease_update_url, _("download update")
+                )), 2, 2)
+        return updates_widget
+    
     def __about_line(self, author, year):
         line = "© "
         if isinstance(year, tuple):
