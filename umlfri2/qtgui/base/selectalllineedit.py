ee6d44721ce6dc1faa092172ccc0aa2f57f23898
Jan Janech <janik@janik.ws>
2017-09-11 16:43:27 +0200

    better focus handling in properties dialog

--- umlfri2/qtgui/base/selectalllineedit.py
+++ umlfri2/qtgui/base/selectalllineedit.py
@@ -1,6 +1,10 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QLineEdit
 
 
 class SelectAllLineEdit(QLineEdit):
     def focusInEvent(self, event):
-        self.selectAll()
+        super().focusInEvent(event)
+        
+        if event.reason() in (Qt.TabFocusReason, Qt.BacktabFocusReason, Qt.ActiveWindowFocusReason):
+            self.selectAll()
