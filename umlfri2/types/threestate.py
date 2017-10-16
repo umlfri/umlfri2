96a4c28cbb7e81cecb0be8e5b9b8f9a367efb4e5
Jan Janech <janik@janik.ws>
2017-10-16 10:35:36 +0200

    better Maybe type support

--- umlfri2/types/threestate.py
+++ umlfri2/types/threestate.py
@@ -1,4 +1,7 @@
 class MaybeType:
+    def __new__(cls, *args, **kwargs):
+        raise Exception("Cannot create a new MaybeType instance")
+
     def __str__(self):
         return "Maybe"
     
@@ -6,16 +9,40 @@ class MaybeType:
         return "Maybe"
     
     def __and__(self, other):
+        if not isinstance(other, (bool, MaybeType)):
+            return NotImplemented
+
         if other is self:
             return self
         else:
             return other
     
     def __rand__(self, other):
+        if not isinstance(other, (bool, MaybeType)):
+            return NotImplemented
+
         if other is self:
             return self
         else:
             return other
 
+    def __or__(self, other):
+        if not isinstance(other, (bool, MaybeType)):
+            return NotImplemented
+
+        if other is True:
+            return True
+        else:
+            return self
+
+    def __ror__(self, other):
+        if not isinstance(other, (bool, MaybeType)):
+            return NotImplemented
+
+        if other is True:
+            return True
+        else:
+            return self
+
     
-Maybe = MaybeType()
+Maybe = object.__new__(MaybeType)
