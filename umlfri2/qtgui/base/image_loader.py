133b247c89107561d5f13322f5abf881d1fef9d2
Jan Janech <janik@janik.ws>
2017-10-27 11:35:14 +0200

    caching metamodel/addon pixmaps in memory

--- umlfri2/qtgui/base/image_loader.py
+++ umlfri2/qtgui/base/image_loader.py
@@ -3,19 +3,28 @@ from weakref import WeakKeyDictionary
 from PyQt5.QtGui import QPixmap, QIcon
 
 
+pixmap_cache = WeakKeyDictionary()
 icon_cache = WeakKeyDictionary()
 
 
+def load_from_cache(cache, image, factory):
+    storage_cache = cache.setdefault(image.storage, {})
+    if image.path in storage_cache:
+        return storage_cache[image.path]
+    
+    ret = factory(image)
+    
+    storage_cache[image.path] = ret
+    return ret
+
+
 def load(image):
-    pix = QPixmap()
-    pix.loadFromData(image.load().read())
-    return pix
+    def factory(image):
+        pix = QPixmap()
+        pix.loadFromData(image.load().read())
+        return pix
+    return load_from_cache(pixmap_cache, image, factory)
 
 
 def load_icon(image):
-    storage_icon_cache = icon_cache.setdefault(image.storage, {})
-    if image.path in storage_icon_cache:
-        return storage_icon_cache[image.path]
-    icon = QIcon(load(image))
-    storage_icon_cache[image.path] = icon
-    return icon
+    return load_from_cache(icon_cache, image, lambda image: QIcon(load(image)))
