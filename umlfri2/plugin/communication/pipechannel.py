ba038e3f0eff3006d7863ee7a1b8482bc6bacb51
Jan Janech <janik@janik.ws>
2018-01-09 15:52:20 +0100

    simpler way to start plugin subprocess

--- umlfri2/plugin/communication/pipechannel.py
+++ umlfri2/plugin/communication/pipechannel.py
@@ -2,6 +2,12 @@ import os
 
 from .filechannel import FileChannel
 
+try:
+    from msvcrt import get_osfhandle
+except ImportError:
+    def get_osfhandle(fd):
+        return fd
+
 
 class PipeChannel(FileChannel):
     def __init__(self):
@@ -17,12 +23,12 @@ class PipeChannel(FileChannel):
         self.__write_child = write_child
     
     @property
-    def child_reader_descriptor(self):
-        return self.__read_child
+    def child_reader_handle(self):
+        return get_osfhandle(self.__read_child)
     
     @property
-    def child_writer_descriptor(self):
-        return self.__write_child
+    def child_writer_handle(self):
+        return get_osfhandle(self.__write_child)
     
     def close_child_descriptors(self):
         os.close(self.__read_child)
--- umlfri2/plugin/starters/base.py
+++ umlfri2/plugin/starters/base.py
@@ -2,13 +2,6 @@ import os
 
 import subprocess
 
-import signal
-
-try:
-    import msvcrt
-except ImportError:
-    pass
-
 from .starter import PluginStarter
 from ..communication import PipeChannel
 
@@ -30,57 +23,29 @@ class BaseProgramStarter(PluginStarter):
         
         program = [part.format(path = self.__path) for part in self.program]
         
-        if os.name == 'nt':
-            ppin = msvcrt.get_osfhandle(channel.child_reader_descriptor)
-            ppout = msvcrt.get_osfhandle(channel.child_writer_descriptor)
-            
-            env['UMLFRI_PIN'] = str(ppin)
-            env['UMLFRI_POUT'] = str(ppout)
-            
-            self.__process = subprocess.Popen(program, close_fds = False, env = env)
-            channel.close_child_descriptors()
-        else:
-            env['UMLFRI_PIN'] = str(channel.child_reader_descriptor)
-            env['UMLFRI_POUT'] = str(channel.child_writer_descriptor)
-            pid = os.fork()
-            if pid:
-                #parent
-                self.__pid = pid
-                channel.close_child_descriptors()
-            else:
-                #child
-                channel.close()
-                os.execve(program[0], program, env)
+        env['UMLFRI_PIN'] = str(channel.child_reader_handle)
+        env['UMLFRI_POUT'] = str(channel.child_writer_handle)
+        
+        self.__process = subprocess.Popen(program, close_fds = False, env = env)
+        
+        channel.close_child_descriptors()
         
         return channel
     
     def terminate(self):
-        if os.name == 'nt':
-            try:
-                self.__process.terminate()
-            except IOError:
-                if self.__process.poll() is None:
-                    raise
-
-        else:
-            os.kill(self.__pid, signal.SIGTERM)
-        
+        try:
+            self.__process.terminate()
+        except IOError:
+            if self.__process.poll() is None:
+                raise
+    
     def kill(self):
-        if os.name == 'nt':
-            try:
-                self.__process.kill()
-            except IOError:
-                if self.__process.poll() is None:
-                    raise
-        else:
-            os.kill(self.__pid, signal.SIGKILL)
+        try:
+            self.__process.kill()
+        except IOError:
+            if self.__process.poll() is None:
+                raise
     
     @property
     def is_alive(self):
-        if os.name == 'nt':
-            return self.__process.poll() is None
-        else:
-            try:
-                return os.waitpid(self.__pid, os.WNOHANG) == (0, 0)
-            except:
-                return False
+        return self.__process.poll() is None
