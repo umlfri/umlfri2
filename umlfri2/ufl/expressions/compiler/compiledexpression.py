9540d48a4bde0eabc7e4a22e7edb738a0cc87a60
Jan Janech <janik@janik.ws>
2018-01-30 09:11:06 +0100

    compiled ufl expression is added to the python line cache to make it included in a excetion traceback

--- umlfri2/ufl/expressions/compiler/compiledexpression.py
+++ umlfri2/ufl/expressions/compiler/compiledexpression.py
@@ -1,4 +1,7 @@
+import linecache
+
 from umlfri2.types.enums import ALL_ENUMS
+
 from .typingvisitor import UflTypingVisitor
 from .compilingvisitor import UflCompilingVisitor
 from ..parser import parse_ufl
@@ -17,11 +20,27 @@ class CompiledUflExpression:
         
         compiling_visitor = UflCompilingVisitor(self.__VARIABLE_PREFIX)
         self.__compiled_source = typed_tree.accept(compiling_visitor)
-        self.__compiled_function = eval(self.compiled_source, compiling_visitor.all_globals)
+        all_globals = compiling_visitor.all_globals
+
+        self.__compiled_function = self.__compile(all_globals)
         
         self.__parameters = tuple(var.name for var in typed_tree.variables)
         
         self.__type = typed_tree.type
+    
+    def __compile(self, all_globals):
+        temp_file_name = "ufl_{0}".format(id(self))
+        
+        to_eval = compile(self.__compiled_source, temp_file_name, "eval")
+        
+        linecache.cache[temp_file_name] = (
+            1,
+            None,
+            ["{0} # ufl: {1}".format(self.__compiled_source, self.__source)],
+            temp_file_name
+        )
+
+        return eval(to_eval, all_globals)
     
     @property
     def source(self):
