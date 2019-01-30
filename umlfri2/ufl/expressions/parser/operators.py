de7d60e5dba983a0c062971e5f84cb8f1306ae07
Jan Janech <jan.janech@kst.uniza.sk>
2019-01-30 15:44:29 +0100

    textual synonyms for logical ufl operators to make it simpler to write ufl expressions in xml

--- umlfri2/ufl/expressions/parser/operators.py
+++ umlfri2/ufl/expressions/parser/operators.py
@@ -31,13 +31,40 @@ BINARY_PRIORITIES = {
     '&&': PriorityGroup.LogicalMultiplicative,
 }
 
-BINARY_OPERATORS = tuple(BINARY_PRIORITIES.keys())
+BINARY_OPERATOR_SYNONYMS = {
+    'gt': '>',
+    'lt': '<',
+    'ge': '>=',
+    'le': '<=',
+    'eq': '==',
+    'ne': '!=',
+    'and': '&&',
+    'or': '||',
+}
 
-UNARY_OPERATORS = ('!', '-', '+')
+BINARY_OPERATORS = tuple(BINARY_PRIORITIES.keys()) + tuple(BINARY_OPERATOR_SYNONYMS.keys())
+
+UNARY_OPERATOR_LIST = ('!', '-', '+')
+
+UNARY_OPERATOR_SYNONYMS = {
+    'not': '!',
+}
+
+UNARY_OPERATORS = tuple(UNARY_OPERATOR_LIST) + tuple(UNARY_OPERATOR_SYNONYMS.keys())
+
+OPERATOR_KEYWORDS = tuple(BINARY_OPERATOR_SYNONYMS.keys()) + tuple(UNARY_OPERATOR_SYNONYMS.keys())
 
 
 def get_priority(operator):
     return BINARY_PRIORITIES[operator].value
+
+
+def get_binary_operator(operator):
+    return BINARY_OPERATOR_SYNONYMS.get(operator, operator)
+
+
+def get_unary_operator(operator):
+    return UNARY_OPERATOR_SYNONYMS.get(operator, operator)
 
 
 operator_tree_stack_node = namedtuple('operator_tree_stack_node', ['value', 'operator'])
@@ -47,6 +74,7 @@ def make_binary_operator_tree(linear, mk_node=lambda a, op, b: (a, op, b)):
     stack = []
     
     for value, operator in zip_longest(islice(linear, 0, None, 2), islice(linear, 1, None, 2)):
+        operator = get_binary_operator(operator)
         while stack and (operator is None or get_priority(stack[-1].operator) <= get_priority(operator)):
             top_value, top_operator = stack.pop(-1)
             value = mk_node(top_value, top_operator, value)
@@ -61,5 +89,6 @@ def make_binary_operator_tree(linear, mk_node=lambda a, op, b: (a, op, b)):
 def make_unary_operator_tree(linear, mk_node=lambda op, a: (op, a)):
     value = linear[-1]
     for operator in islice(reversed(linear), 1, None):
+        operator = get_unary_operator(operator)
         value = mk_node(operator, value)
     return value
