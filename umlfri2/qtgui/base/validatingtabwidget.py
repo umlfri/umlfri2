021599152367f033775da11d7d07d5ee6641aab1
Jan Janech <janik@janik.ws>
2020-01-16 13:28:36 +0100

    changing tabs by ctrl+(shift+)tab in property dialog is causing validation too #5

--- umlfri2/qtgui/base/validatingtabwidget.py
+++ umlfri2/qtgui/base/validatingtabwidget.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from PyQt5.QtCore import pyqtSignal, Qt
 from PyQt5.QtWidgets import QTabBar, QTabWidget
 
@@ -45,18 +47,29 @@ class ValidatingTabBar(QTabBar):
             return
         
         if delta > 0:
+            self.tabBar().change_tab_in_direction(to_right=True)
+        else:
+            self.tabBar().change_tab_in_direction(to_right=False)
+        
+        event.ignore()
+    
+    def change_tab_in_direction(self, to_right, continue_from_end=False):
+        if to_right:
             tested_tabs = range(self.currentIndex() - 1, -1, -1)
+            from_start = range(self.count() - 1, self.currentIndex(), -1)
         else:
             tested_tabs = range(self.currentIndex() + 1, self.count(), 1)
+            from_start = range(0, self.currentIndex(), 1)
         
+        if continue_from_end:
+            tested_tabs = chain(tested_tabs, from_start)
+
         for tested_index in tested_tabs:
             if self.isTabEnabled(tested_index):
                 if self.__do_test(tested_index):
                     self.setCurrentIndex(tested_index)
                 break
-        
-        event.ignore()
-
+    
     def __do_test(self, tab_index):
         evt = ValidateTabChangeEvent(tab_index)
         self.validate_tab_change.emit(evt)
@@ -67,3 +80,13 @@ class ValidatingTabWidget(QTabWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setTabBar(ValidatingTabBar())
+    
+    def keyPressEvent(self, event):
+        if event.key() in (Qt.Key_Tab, Qt.Key_Backtab) and event.modifiers() & Qt.ControlModifier:
+            if event.key() == Qt.Key_Backtab or event.modifiers() & Qt.ShiftModifier:
+                self.tabBar().change_tab_in_direction(to_right=True, continue_from_end=True)
+            else:
+                self.tabBar().change_tab_in_direction(to_right=False, continue_from_end=True)
+            event.ignore()
+        else:
+            super().keyPressEvent(event)
