23224dbc8f1068b5ff886866270f7196ac58a8a5
Jan Janech <janik@janik.ws>
2020-01-16 09:13:37 +0100

    propertydialog validation started when tabs are changed using a mouse wheel #5

--- umlfri2/qtgui/base/validatingtabbar.py
+++ umlfri2/qtgui/base/validatingtabbar.py
@@ -30,8 +30,34 @@ class ValidatingTabBar(QTabBar):
         if tab_index < 0 or tab_index == self.currentIndex():
             return super().mousePressEvent(event)
 
-        evt = ValidateTabChangeEvent(tab_index)
-        self.validate_tab_change.emit(evt)
-        if evt.is_valid:
+        if self.__do_test(tab_index):
             return super().mousePressEvent(event)
         event.ignore()
+    
+    def wheelEvent(self, event):
+        delta_point = event.angleDelta()
+        if delta_point.x() == 0 and delta_point.y() != 0:
+            delta = delta_point.y()
+        elif delta_point.x() != 0 and delta_point.y() == 0:
+            delta = delta_point.x()
+        else:
+            event.ignore()
+            return
+        
+        if delta < 0:
+            tested_tabs = range(self.currentIndex() - 1, -1, -1)
+        else:
+            tested_tabs = range(self.currentIndex() + 1, self.count(), 1)
+        
+        for tested_index in tested_tabs:
+            if self.isTabEnabled(tested_index):
+                if self.__do_test(tested_index):
+                    self.setCurrentIndex(tested_index)
+                break
+        
+        event.ignore()
+
+    def __do_test(self, tab_index):
+        evt = ValidateTabChangeEvent(tab_index)
+        self.validate_tab_change.emit(evt)
+        return evt.is_valid
